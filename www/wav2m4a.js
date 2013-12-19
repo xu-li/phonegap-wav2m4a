4217e751735e0041d716322e4f4ed486d0ed64cb
xu.li <AthenaLightenedMyPath@gmail.com>
2013-12-19 16:51:09 +0800

    remove swp file and add explaination

--- www/wav2m4a.js
+++ www/wav2m4a.js
@@ -5,6 +5,8 @@ var exec = require('cordova/exec');
 
 /**
  * Convert the wav to m4a
+ *
+ * Usage: wav2m4a.convert(src, target, function () { alert('Success!'); });
  */
 function convert(source, target, success, fail) {
     exec(success, fail, "Wav2M4a", "convert", [source, target]);
